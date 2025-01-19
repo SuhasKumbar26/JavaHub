e290689a06a9865c6cc9f08779434f444e52bb72
Suhas Kumbar <suhaskumbar1043@gmail.com>
2025-01-19 10:03:22 +0530

    Introduction to Java

---
+++ Lecture Code/1 Introduction to java/src/historyOfJava.java
@@ -0,0 +1,12 @@
+public class historyOfJava {
+    public static void main(String[] args){
+        System.out.println(
+                "\nJava was developed by the James gosling at Sun Micro system\n" +
+                "First named as OAK later converted into JAVA and first released in 1995\n" +
+                "It provides backward compatibility\n" +
+                "Interpreter --> step by step code conversion\n" +
+                "Compiler --> Converts whole code at time\n" +
+                "program.java(SourceCode) --> program.class(ByteCode) --> MachineCode(BinaryCode)\n"
+        );
+    }
+}
