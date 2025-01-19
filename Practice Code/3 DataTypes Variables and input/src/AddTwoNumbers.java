1718a9d0ed5ce8ff41ab776ecf359c739c6fe7a1
Suhas Kumbar <suhaskumbar1043@gmail.com>
2025-01-19 15:59:45 +0530

    Add two numbers

---
+++ Practice Code/3 DataTypes Variables and input/src/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1+num2;
+        System.out.println("Sum: " +sum);
+    }
+}
