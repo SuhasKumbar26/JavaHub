b03bf5c5a0685e2216d472e0f51250534a9905b4
Suhas Kumbar <suhaskumbar1043@gmail.com>
2025-01-20 17:25:42 +0530

    Assignment Operator

---
+++ Lecture Code/4 Operators if-else and Number System/src/Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment {
+    public static void main(String[] args){
+        int number = 123;
+        int myNum = number;
+
+        System.out.print(number);
+        System.out.print(myNum);
+    }
+}
