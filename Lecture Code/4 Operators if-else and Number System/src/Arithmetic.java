94d0aa64cc89ba8da0ab65762f4f205b0e4ea2e1
Suhas Kumbar <suhaskumbar1043@gmail.com>
2025-01-20 17:24:39 +0530

    Arithmetic Operator

---
+++ Lecture Code/4 Operators if-else and Number System/src/Arithmetic.java
@@ -0,0 +1,21 @@
+public class Arithmetic {
+        public static void main(String[] args){
+
+            int a = 8;
+            int b = 5;
+            System.out.println("a+b: "+(a+b));
+            System.out.println("a-b: "+(a-b));
+            System.out.println("a*b: "+(a*b));
+            System.out.println("a/b: "+(a/b));
+            System.out.println("a%b: "+(a%b));
+
+            double x = 5;
+            double y = 2;
+            System.out.println("x+y: "+(x + y));
+            System.out.println("x-y: "+(x - y));
+            System.out.println("x*y: "+(x * y));
+            System.out.println("x/y: "+(x / y));
+            System.out.println("x%y: "+(x % y));
+
+        }
+}
