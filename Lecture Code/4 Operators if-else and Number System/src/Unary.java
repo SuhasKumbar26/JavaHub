0a36db604a7383f046922da4f2e4747b21e76c81
Suhas Kumbar <suhaskumbar1043@gmail.com>
2025-01-20 17:27:09 +0530

    Unary Operator

---
+++ Lecture Code/4 Operators if-else and Number System/src/Unary.java
@@ -0,0 +1,12 @@
+public class Unary {
+    public static void main(String[] args){
+        int x = 5;
+        int y = -x;
+        System.out.println(y);
+
+        System.out.println(x++); //x = 5 --> x = 6
+
+        System.out.println(++x); //x = 7
+
+    }
+}
